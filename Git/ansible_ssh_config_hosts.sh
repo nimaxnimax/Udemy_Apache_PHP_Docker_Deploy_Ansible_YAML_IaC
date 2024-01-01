b02b6d519a6212bb10028bff56352e4c81f0c852
nimaxnimax <avaroti.service@gmail.com>
2024-01-01 13:10:38 +0100

    done

--- Git/ansible_ssh_config_hosts.sh
+++ Git/ansible_ssh_config_hosts.sh
@@ -1,3 +1,10 @@
 ssh-keygen
-ssh-copy-id ubuntu1
-ssh-copy-id ubuntu2
+
+# example >> ssh-copy-id >> copy and install key >> server >> ubuntu1 & 2 >> username >> adrian
+ssh-copy-id adrian@ubuntu1
+ssh-copy-id adrian@ubuntu2
+
+# to test and verify >> ssh using username >> example: adrian username
+ssh adrian@ubuntu1
+ssh adrian@ubuntu2
+
